09f91cc521257bea3673a6b1348ee91ee6b6b6d5
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2023-11-09 12:54:09 -0300

    Update ex010.py

--- Exercícios/ex010.py
+++ Exercícios/ex010.py
@@ -2,3 +2,4 @@ valor_real = float(input('Informe o valor: R$'))
 valor_dolar = valor_real / 5.22
 
 print('Com R${} você conseguirar US${:.2f}'.format(valor_real, valor_dolar))
+print('Olá, mundo!')
